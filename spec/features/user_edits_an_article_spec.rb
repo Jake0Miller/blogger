81f56a5fa91223bbb58aafd52c88deb48e08a2e0
Jake Miller <jake0miller@gmail.com>
2019-06-17 23:17:56 -0600

    Write edit functionality, all tests pass

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "user edits an article" do
+  describe "they link from the show page" do
+    describe "they fill in new title and body" do
+      it "displays the new article on a show page" do
+        article = Article.create!(title: "Title 1", body: "Body 1")
+
+        visit article_path(article)
+        click_link "Edit"
+
+        fill_in "article[title]", with: "New Title!"
+        fill_in "article[body]", with: "New Body!"
+        click_on "Update Article"
+
+        expect(page).to have_content("New Title!")
+        expect(page).to have_content("New Body!")
+      end
+    end
+  end
+end
