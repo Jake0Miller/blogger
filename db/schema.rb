66437c7ca2e189760703ef414c8cf84dfcdca736
Jake Miller <jake0miller@gmail.com>
2019-06-22 01:36:54 -0600

    Add paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190620071030) do
+ActiveRecord::Schema.define(version: 20190622065216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20190620071030) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -34,16 +38,12 @@ ActiveRecord::Schema.define(version: 20190620071030) do
   create_table "taggings", force: :cascade do |t|
     t.bigint "tag_id"
     t.bigint "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_taggings_on_article_id"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
